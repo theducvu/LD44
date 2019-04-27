3b56c077d61894685430f3eca1d2600430020bf0
theducvu <vutheduc96@gmail.com>
2019-04-27 20:54:05 +1000

    Initial Commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+}
